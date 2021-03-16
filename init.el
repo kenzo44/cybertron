189d626bac05fdff18ed63be9211623557f5ecc0
KenzoPi <kenish.suri@yahoo.com>
2021-03-16 15:40:28 -0600

    Added Package Manger

--- init.el
+++ init.el
@@ -1,4 +1,4 @@
-;Visual Tweaks
+;; Visual Tweaks
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
 (tooltip-mode -1)
@@ -6,3 +6,38 @@
 (menu-bar-mode -1)
 (setq visible-bell t)
 
+(column-number-mode)
+(global-display-line-numbers-mode t)
+
+;; Default theme temp
+(load-theme 'tango-dark)
+
+;; Initialize package sources
+(require 'package)
+
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("org" . "https://orgmode.org/elpa/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
+
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+
+;; Initialize use-package on non-Linux platforms
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure t)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
