288624583100fa39362e4243fae067386d4fcab0
KenzoPi <kenish.suri@yahoo.com>
2021-03-22 01:04:16 -0600

    Base transformation complete

--- init.el
+++ init.el
@@ -1,4 +1,8 @@
-;;; Configuration --- Project Cybertron
+;;; init.el --- Project Cybertron
+;; Author: Kenish
+;;; Commentary:
+;;; Not sure ye
+;;; Code:
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
 (tooltip-mode -1)
@@ -7,13 +11,49 @@
 (fset 'yes-or-no-p 'y-or-n-p)
 (setq visible-bell t)
 (display-time-mode t)
-
+(setq-default default-directory "~/")
 (setenv "BROWSER" "firefox")t
 
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
 
+;; Initialize package sources
+(require 'package)
+
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("org" . "https://orgmode.org/elpa/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
+
+(package-initialize)
+(unless package-archive-contents
+ (package-refresh-contents))
+
+;; Initialize use-package on non-Linux platforms
+(unless (package-installed-p 'use-package)
+   (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+(use-package "window"
+  :ensure nil
+  :preface
+  (defun ian/split-and-follow-horizontally ()
+    "Split window below."
+    (interactive)
+    (split-window-below)
+    (other-window 1))
+  (defun ian/split-and-follow-vertically ()
+    "Split window right."
+    (interactive)
+    (split-window-right)
+    (other-window 1))
+  :config
+  (setq split-width-threshold 100)
+  (global-set-key (kbd "C-x 2") #'ian/split-and-follow-horizontally)
+  (global-set-key (kbd "C-x 3") #'ian/split-and-follow-vertically))
+
 (column-number-mode)
-(global-display-line-numbers-mode t)
+(setq display-line-numbers 'relative)
 ;; Overwrite when pasting
 (delete-selection-mode 1)
 ;; Wrap lines
@@ -24,7 +64,129 @@
 
 ;; Quality of Life
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
-(use-package nyan-mode :ensure t)
+
+;; Searching/sorting enhancements & project management
+
+(use-package ivy
+  :hook (after-init . ivy-mode)
+  :config
+  (setcdr (assoc t ivy-format-functions-alist) #'ivy-format-function-line)
+  (setq ivy-height 12)
+  (setq ivy-display-style nil)
+  (setq ivy-re-builders-alist
+        '((counsel-rg            . ivy--regex-plus)
+          (counsel-projectile-rg . ivy--regex-plus)
+          (swiper                . ivy--regex-plus)
+          (t                     . ivy--regex-fuzzy)))
+  (setq ivy-use-virtual-buffers t)
+  (setq ivy-count-format "(%d/%d) ")
+  (setq ivy-initial-inputs-alist nil)
+  (define-key ivy-minibuffer-map (kbd "RET") #'ivy-alt-done)
+  (define-key ivy-mode-map       (kbd "<escape>") nil)
+  (define-key ivy-minibuffer-map (kbd "<escape>") #'minibuffer-keyboard-quit))
+
+(use-package counsel
+  :hook (ivy-mode . counsel-mode)
+  :config
+  (setq counsel-rg-base-command "rg --vimgrep %s")
+  (global-set-key (kbd "s-P") #'counsel-M-x)
+  (global-set-key (kbd "C-S-p") #'counsel-M-x)
+  (global-set-key (kbd "s-f") #'counsel-grep-or-swiper)
+  (global-set-key (kbd "C-s") #'counsel-grep-or-swiper))
+
+(use-package counsel-projectile
+  :config
+  (counsel-projectile-mode +1))
+
+(use-package swiper
+  :after ivy
+  :config
+  (setq swiper-action-recenter t)
+  (setq swiper-goto-start-of-match t))
+
+(use-package projectile
+  :config
+  (setq projectile-sort-order 'recentf)
+  (setq projectile-indexing-method 'hybrid)
+  (setq projectile-completion-system 'ivy)
+  (setq projectile-mode-line-prefix " ")
+  (projectile-mode +1)
+  (define-key projectile-mode-map (kbd "C-c p") #'projectile-command-map)
+  (define-key projectile-mode-map (kbd "s-p") #'projectile-find-file)
+  (define-key projectile-mode-map (kbd "C-p") #'projectile-find-file)
+  (define-key projectile-mode-map (kbd "s-F") #'projectile-ripgrep)
+  (define-key projectile-mode-map (kbd "C-S-f") #'projectile-ripgrep))
+
+(use-package ivy-prescient
+  :after (prescient ivy counsel)
+  :config
+  (setq ivy-prescient-sort-commands
+        '(:not swiper
+               counsel-grep
+               counsel-rg
+               counsel-projectile-rg
+               ivy-switch-buffer
+               counsel-switch-buffer))
+  (setq ivy-prescient-retain-classic-highlighting t)
+  (ivy-prescient-mode +1))
+
+(use-package company-prescient
+  :after (prescient company)
+  :config
+  (company-prescient-mode +1))
+
+(use-package ivy-rich
+  :preface
+  (defun ivy-rich-switch-buffer-icon (candidate)
+    (with-current-buffer
+        (get-buffer candidate)
+      (all-the-icons-icon-for-mode major-mode)))
+  :init
+  (setq ivy-rich-display-transformers-list ; max column width sum = (ivy-poframe-width - 1)
+        '(ivy-switch-buffer
+          (:columns
+           ((ivy-rich-switch-buffer-icon (:width 2))
+            (ivy-rich-candidate (:width 35))
+            (ivy-rich-switch-buffer-project (:width 15 :face success))
+            (ivy-rich-switch-buffer-major-mode (:width 13 :face warning)))
+           :predicate
+           #'(lambda (cand) (get-buffer cand)))
+          counsel-M-x
+          (:columns
+           ((counsel-M-x-transformer (:width 35))
+            (ivy-rich-counsel-function-docstring (:width 34 :face font-lock-doc-face))))
+          counsel-describe-function
+          (:columns
+           ((counsel-describe-function-transformer (:width 35))
+            (ivy-rich-counsel-function-docstring (:width 34 :face font-lock-doc-face))))
+          counsel-describe-variable
+          (:columns
+           ((counsel-describe-variable-transformer (:width 35))
+            (ivy-rich-counsel-variable-docstring (:width 34 :face font-lock-doc-face))))
+          package-install
+          (:columns
+           ((ivy-rich-candidate (:width 25))
+            (ivy-rich-package-version (:width 12 :face font-lock-comment-face))
+            (ivy-rich-package-archive-summary (:width 7 :face font-lock-builtin-face))
+            (ivy-rich-package-install-summary (:width 23 :face font-lock-doc-face))))))
+  :config
+  (ivy-rich-mode +1)
+  (setcdr (assq t ivy-format-functions-alist) #'ivy-format-function-line))
+
+(use-package ivy-posframe
+  :demand t
+  :after ivy
+  :custom
+  (ivy-posframe-display-functions-alist '((t . ivy-posframe-display-at-frame-center)))
+  :config
+  (ivy-posframe-mode))
+
+(use-package nyan-mode
+  :ensure t
+  :config
+  (setq nyan-wavy-trail 1)
+  (nyan-mode))
+
 ;; Which key
 (use-package which-key
   :ensure t
@@ -44,7 +206,6 @@
 
 ;; Ivy Mode
 (ivy-mode)
-(setq ivy-use-virtual-buffers t)
 (setq enable-recursive-minibuffers t)
 ;; enable this if you want `swiper' to use it
 ;; (setq search-default-mode #'char-fold-to-regexp)
@@ -61,7 +222,6 @@
   (setq company-minimum-prefix-length 3)
   (global-company-mode t))
 
-
 ;; Flycheck
 (use-package flycheck
   :ensure t
@@ -74,31 +234,13 @@
   :config
   (add-hook 'clojure-mode-hook #'rainbow-delimiters-mode))
 (show-paren-mode t)
-
-;; Initialize package sources
-(require 'package)
-
-(setq package-archives '(("melpa" . "https://melpa.org/packages/")
-                         ("org" . "https://orgmode.org/elpa/")
-                         ("elpa" . "https://elpa.gnu.org/packages/")))
-
-(package-initialize)
-(unless package-archive-contents
-  (package-refresh-contents))
-
-;; Initialize use-package on non-Linux platforms
-(unless (package-installed-p 'use-package)
-  (package-install 'use-package))
-
-(require 'use-package)
-(setq use-package-always-ensure t)
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(nyan-mode company counsel aggressive-indent rainbow-delimiters flycheck chess which-key doom-modeline ivy use-package)))
+   '(rainbow-delimiters flycheck aggressive-indent doom-modeline which-key nyan-mode ivy-posframe ivy-rich company-prescient ivy-prescient counsel-projectile counsel ivy use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
