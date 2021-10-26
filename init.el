dc4e6cf863f9c540403ae3f6ec2ceafb365593d4
kenzo44 <kenzo44@users.noreply.github.com>
2021-10-25 22:56:27 -0600

    Renamed myinit.org to bwoah.org to fit icemacs theme

--- init.el
+++ init.el
@@ -10,13 +10,6 @@
   (load custom-file))
 
 ;; Org hook
-;; Don't attempt to find/apply special file handlers to files loaded during startup.
-(let ((file-name-handler-alist nil))
-  ;; If config is pre-compiled, then load that
-  (if (file-exists-p (expand-file-name "bwoah.elc" user-emacs-directory))
-      (load-file (expand-file-name "bwoah.elc" user-emacs-directory))
-    ;; Otherwise use org-babel to tangle and load the configuration
-    (require 'org)
-    (org-babel-load-file (expand-file-name "bwoah.org" user-emacs-directory))))
+(org-babel-load-file (expand-file-name "bwoah.org" user-emacs-directory))
 
 ;;; init.el ends here
