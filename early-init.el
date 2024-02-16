0b90d6d20747827e84a6923b63d0ff7546d7e085
kenzo44 <kenish.suri@yahoo.com>
2024-02-15 23:33:15 -0700

    A new beginning

--- early-init.el
+++ early-init.el
@@ -1,51 +1,42 @@
 ;;; early-init.el -*- lexical-binding: t; -*-
-;; Emacs 27.1 introduced early-init.el, which is run before init.el, before
-;; package and UI initialization happens, and before site files are loaded.
-(when (version< emacs-version "25.1")
-  (error "Icemacs requires Emacs 25.1 and above!"))
-;; Increase the gc-cons-threshold to a very high number to decrease the load time
-(eval-and-compile
-  (setq gc-cons-threshold 402653184
-        gc-cons-percentage 0.6))
-;; Add a hook to reset this value after initialization
-(add-hook 'icemacs-post-init-hook #'(lambda () (setq gc-cons-threshold 16777216
-                                                   gc-cons-percentage 0.1)))
-;;; Package settings
-(setq package-enable-at-startup nil
-      inhibit-startup-message   t
-      frame-resize-pixelwise    t  ; fine resize
-      package-native-compile    t) ; native compile packages
-;; UI - Disable visual cruft
-;; Prevent the glimpse of un-styled Emacs by disabling these UI elements early.
-(scroll-bar-mode -1)
-(tool-bar-mode -1)
-(tooltip-mode -1)
-(menu-bar-mode -1)
-(setq initial-major-mode 'emacs-lisp-mode)
-(set-default-coding-systems 'utf-8)
-(setq-default default-directory "~/")
-(setq default-frame-alist
-      (append (list
-               '(internal-border-width . 20)
-               '(left-fringe    . 0)
-               '(right-fringe   . 0)
-               '(tool-bar-lines . 0)
-               '(menu-bar-lines . 0)
-               '(vertical-scroll-bars . nil)
-               '(horizontal-scroll-bars . nil)
-               '(height . 45)
-               '(width . 85)
-               )))
 
-(add-to-list 'default-frame-alist '(undecorated . t))
-;; echo buffer
-;; Don't display any message
-;; https://emacs.stackexchange.com/a/437/11934
-(defun display-startup-echo-area-message ()
-  (message ""))
-;; And bury the scratch buffer, don't kill it
-(defadvice kill-buffer (around kill-buffer-around-advice activate)
-  (let ((buffer-to-kill (ad-get-arg 0)))
-    (if (equal buffer-to-kill "*scratch*")
-        (bury-buffer)
-      ad-do-it)))
+;; Optimize garbage collection during startup
+(setq gc-cons-threshold #x40000000)  ; Temporarily disable GC by setting it to ~1GB
+
+;; Temporarily disable the file name handler alist
+(defvar default-file-name-handler-alist file-name-handler-alist)
+(setq file-name-handler-alist nil)
+
+;; Set the initial major mode to fundamental-mode to reduce startup overhead
+(setq initial-major-mode 'fundamental-mode)
+
+;; Restore settings and remove UI clutter after startup
+(add-hook 'emacs-startup-hook
+          (lambda ()
+            ;; Restore the file name handler alist
+            (setq file-name-handler-alist default-file-name-handler-alist)
+            
+            ;; Restore garbage collection thresholds
+            (setq gc-cons-threshold 100000000  ; 100MB
+                  gc-cons-percentage 0.1)
+            
+            ;; Garbage collect unreferenced objects
+            (garbage-collect)
+
+            ;; Remove UI clutter
+            (menu-bar-mode -1)
+            (tool-bar-mode -1)
+            (scroll-bar-mode -1)))
+
+;; Native compilation settings
+(when (featurep 'native-compile)
+  ;; Set the right directory to store the native compilation cache
+  (let ((path (expand-file-name "eln-cache/" user-emacs-directory)))
+    (setq-default native-comp-eln-load-path       (list path)
+                  native-compile-target-directory path)
+    (when (fboundp 'startup-redirect-eln-cache)
+      (startup-redirect-eln-cache path)))
+  (setq-default native-comp-async-report-warnings-errors nil  ;; Silence compiler warnings as they can be pretty disruptive
+                native-comp-deferred-compilation         t    ;; Make native compilation happens asynchronously
+                package-native-compile                   t)   ;; Compile installed packages
+  )
