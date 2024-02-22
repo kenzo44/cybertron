fec54a01c99ed7626c284e07e0db5bfcb9e592dd
kenzo44 <kenish.suri@yahoo.com>
2024-02-21 22:44:43 -0700

    Optimized startup. 29 compatible

--- early-init.el
+++ early-init.el
@@ -2,10 +2,34 @@
 
 ;; Optimize garbage collection during startup
 (setq gc-cons-threshold #x40000000)  ; Temporarily disable GC by setting it to ~1GB
+(setq read-process-output-max (* 1024 1024)) ; A Higher Read Process Maximum
 
 ;; Temporarily disable the file name handler alist
 (defvar default-file-name-handler-alist file-name-handler-alist)
 (setq file-name-handler-alist nil)
+
+;; Remove UI clutter
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(tooltip-mode -1)
+
+;; Default frame configuration: full screen, good-looking title bar on macOS
+(setq frame-resize-pixelwise t)
+
+;; Ensure that fullscreen and other frame-related settings are correctly set
+(setq default-frame-alist
+      (append default-frame-alist
+              '((fullscreen . maximized) ; Fullscreen mode
+                (internal-border-width . 20) ; Set internal border width
+                (background-color . "#000000")
+                )))
+
+(when (eq system-type 'darwin)
+  ;; macOS-specific settings
+  (add-to-list 'default-frame-alist '(ns-appearance . dark)) ; Dark mode appearance
+  (add-to-list 'default-frame-alist '(ns-transparent-titlebar . t)) ; Transparent title bar
+  (add-to-list 'default-frame-alist '(alpha . (95 . 95))))
 
 ;; Set the initial major mode to fundamental-mode to reduce startup overhead
 (setq initial-major-mode 'fundamental-mode)
@@ -23,10 +47,9 @@
             ;; Garbage collect unreferenced objects
             (garbage-collect)
 
-            ;; Remove UI clutter
-            (menu-bar-mode -1)
-            (tool-bar-mode -1)
-            (scroll-bar-mode -1)))
+	     ;; Defaults
+	    (set-default-coding-systems 'utf-8)
+	    (setq-default default-directory "~/")))
 
 ;; Native compilation settings
 (when (featurep 'native-compile)
